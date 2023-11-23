7f71a88c5c67c5d2a540cf228248fba82514c58e
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2023-11-23 22:27:34 +0530

    Add files via upload

---
+++ ghome.jsx
@@ -0,0 +1,305 @@
+import React from "react";
+import SAka from "/image/Saka.jpg";
+import Nddd from "/image/nddd.jpg";
+
+function ghome() {
+  return (
+    <div style={{ position: "absolute", backgroundColor: "#ffffff" }}>
+      <header>
+        <span
+          style={{
+            top: "15px",
+            position: "absolute",
+            left: "180px",
+            fontFamily: "cursive",
+            fontSize: "20px",
+            color: "#515083",
+          }}
+        >
+          Travellogo
+        </span>
+        <div style={{ left: "600px", top: "15px", position: "absolute" }}>
+          <span
+            style={{
+              position: "absolute",
+              left: "300px",
+              whiteSpace: "nowrap",
+              color: "#6f6f7f",
+            }}
+            onMouseOver={(e) => (e.target.style.color = "#f4a642")}
+            onMouseOut={(e) => (e.target.style.color = "#6f6f7f")}
+          >
+            Home
+          </span>
+          <span
+            style={{
+              position: "absolute",
+              left: "400px",
+              whiteSpace: "nowrap",
+              color: "#6f6f7f",
+            }}
+            onMouseOver={(e) => (e.target.style.color = "#f4a642")}
+            onMouseOut={(e) => (e.target.style.color = "#6f6f7f")}
+          >
+            About
+          </span>
+          <span
+            style={{
+              position: "absolute",
+              left: "480px",
+              whiteSpace: "nowrap",
+              color: "#6f6f7f",
+            }}
+            onMouseOver={(e) => (e.target.style.color = "#f4a642")}
+            onMouseOut={(e) => (e.target.style.color = "#6f6f7f")}
+          >
+            Tour Packges
+          </span>
+          <span
+            style={{
+              position: "absolute",
+              left: "600px",
+              whiteSpace: "nowrap",
+              color: "#6f6f7f",
+            }}
+            onMouseOver={(e) => (e.target.style.color = "#f4a642")}
+            onMouseOut={(e) => (e.target.style.color = "#6f6f7f")}
+          >
+            Contact Us
+          </span>
+        </div>
+        <button
+          style={{
+            position: "absolute",
+            left: "1300px",
+            whiteSpace: "nowrap",
+            borderRadius: "200px",
+            border: "none",
+            backgroundColor: "#f4a642",
+            color: "white",
+            width: "100px",
+            height: "40px",
+            top: "10px",
+            fontFamily: "cursive",
+          }}
+        >
+          Book Tiket
+        </button>
+      </header>
+      <div style={{ position: "absolute", top: "550px" }}>
+        <img
+          src={SAka}
+          alt="user"
+          style={{
+            width: "600px",
+            height: "400px",
+            left: "180px",
+            position: "absolute",
+        
+          }}
+        />
+        <h1
+          style={{
+            position: "absolute",
+            top: "30px",
+            left: "800px",
+            fontFamily: "andalus",
+            fontSize: "40px",
+            color: "#6f6d86",
+            width: "600px",
+          }}
+        >
+          Take The Best Weekend And Enjon Your Place
+        </h1>
+        <h1
+          style={{
+            position: "absolute",
+            top: "140px",
+            left: "800px",
+            fontFamily: "andalus",
+            fontSize: "13px",
+            color: "#c5c5cb",
+            width: "600px",
+          }}
+        >
+          A paragraph is a self-contained unit of discourse in writing dealing
+          with a particular point or idea. Though not required by the
+          orthographic conventions of any language with a writing system,
+          paragraphs are a conventional means of organizing extended segments of
+          prose. A paragraph is a self-contained unit of discourse in writing
+          dealing with a particular point or idea. Though not required by the
+          orthographic conventions of any language with a writing system,
+          paragraphs are a conventional means of organizing extended segments of
+          prose. A paragraph is a self-contained unit of discourse in writing
+          dealing with a particular point or idea. Though not required by the
+          orthographic conventions of any language with a writing system,
+          paragraphs are a conventional means of organizing extended segments of
+          prose.prose. A paragraph is a self-contained unit of discourse in writing
+          dealing with a particular point or idea. Though not required by the
+          orthographic conventions of any language with a writing system,
+          paragraphs are a conventional means of organizing extended segments of
+          
+        </h1>
+        <button
+          style={{
+            position: "absolute",
+            top: "320px",
+            left: "800px",
+            width: "120px",
+            height: "50px",
+            backgroundColor: "#f4a641",
+            color: "white",
+            border: "none",
+            borderRadius: "30px",
+            fontFamily: '-moz-initial'
+          }}
+        >
+          Book Now
+        </button>
+      </div>
+      <div style={{ position: "absolute", top: "60px" }}>
+        <img
+          src={Nddd}
+          alt="user"
+          style={{
+            width: "1520px",
+            height: "420px",
+            left: "",
+            position: "absolute",
+          }}
+        />
+        <h1
+          style={{
+            position: "absolute",
+            width: "400px",
+            color: "#6d6c92",
+            top: "80px",
+            left: "80px",
+            fontSize: "",
+            fontFamily: "-moz-initial",
+          }}
+        >
+          Travel to the any corner of the world.
+        </h1>
+        <h1
+          style={{
+            position: "absolute",
+            width: "400px",
+            color: "#6d6c92",
+            top: "180px",
+            left: "80px",
+            fontSize: "12px",
+            fontFamily: "-moz-initial",
+          }}
+        >
+          Participate online through e-Procurement website for Municipal
+          Corporation Gwalior tenders in Madhya Pradesh. Download Tender
+          Documents, BOQ, Technical Bids fo
+        </h1>
+        <card
+          style={{
+            position: "absolute",
+            top: "360px",
+            left: "200px",
+            width: "600px",
+            height: "100px",
+            backgroundColor: "white",
+            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            borderRadius: "20px",
+          }}
+        >
+          <span
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "40px",
+              fontFamily: "andalus",
+              fontSize: "20px",
+              color: "#f4a641",
+            }}
+          >
+            Where
+          </span>
+          <div
+            style={{
+              position: "absolute",
+              top: "50px",
+              left: "45px",
+              fontSize: "10px",
+              fontFamily: "andalus",
+              color: "#babac1",
+            }}
+          >
+            New Yourk
+          </div>
+          <span
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "200px",
+              fontFamily: "andalus",
+              fontSize: "20px",
+              color: "#595889",
+            }}
+          >
+            Date
+          </span>
+          <div
+            style={{
+              position: "absolute",
+              top: "50px",
+              left: "200px",
+              fontSize: "10px",
+              fontFamily: "andalus",
+              color: "#babac1",
+            }}
+          >
+            Select Date
+          </div>
+          <span
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "350px",
+              fontFamily: "andalus",
+              fontSize: "20px",
+              color: "#595889",
+            }}
+          >
+            Gustes
+          </span>
+          <div
+            style={{
+              position: "absolute",
+              top: "50px",
+              left: "350px",
+              fontSize: "10px",
+              fontFamily: "andalus",
+              color: "#babac1",
+            }}
+          >
+            Select Person
+          </div>
+          <button
+            style={{
+              position: "absolute",
+              top: "35px",
+              left: "480px",
+              fontSize: "10px",
+              fontFamily: "andalus",
+              color: "white",
+              width: "80px",
+              height: "30px",
+              border: "none",
+              borderRadius: "30px",
+              backgroundColor: "#f4a641",
+            }}
+          >
+            Sreach
+          </button>
+        </card>
+      </div>
+    </div>
+  );
+}
+export default ghome;
